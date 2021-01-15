5daab15de52eb10605e23e2da1b0f768f7e056fc
JonathanCheli <57296816+JonathanCheli@users.noreply.github.com>
2021-01-15 04:25:12 -0300

    Update FavDB.java

--- app/src/main/java/com/example/likesfirebaseexample/Helpers/FavDB.java
+++ app/src/main/java/com/example/likesfirebaseexample/Helpers/FavDB.java
@@ -10,7 +10,7 @@ import android.util.Log;
 public class FavDB extends SQLiteOpenHelper {
 
     private static int DB_VERSION = 1;
-    private static String DATABASE_NAME = "CoffeeDB";
+    private static String DATABASE_NAME = "ProductDB";
     private static String TABLE_NAME = "favoriteTable";
     public static String KEY_ID = "id";
     public static String ITEM_TITLE = "itemTitle";
